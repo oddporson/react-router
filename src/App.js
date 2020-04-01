428dc099975d770c2dcc8f40544b6e80fafcc478
Porson <porsonlee@gmail.com>
2020-03-31 22:45:35 -0700

    used Link to link to the route path to render the component

--- src/App.js
+++ src/App.js
@@ -6,13 +6,26 @@ import Shop from './Shop';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
 function App() {
+
+
   return (
-    <div className="App">
-      <Nav /> 
-      <About />
-      <Shop />
-    </div>
+    <Router> 
+      <div className="App">
+        <Nav /> 
+        <Switch>
+          <Route path ="/" exact component={Home} />
+          <Route path="/about" component={About} />
+          <Route path="/shop" component={Shop} />
+        </Switch>
+      </div>
+    </Router>
   );
 }
+
+const Home = () => (
+  <div>
+    <h1>Home Page</h1>
+  </div>
+)
 
 export default App;
